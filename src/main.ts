3f21c8e9f3fbdc00f80d25ebc5996c56afb5de8a
KingFillari <gebrefillimon@gmail.com>
2026-03-13 16:20:41 +0300

    Updated main and the gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Global security and parsing
+  app.use(helmet());
+  app.use(cors());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Referral Backend API running on http://localhost:${port}`);
 }
+
 bootstrap();
