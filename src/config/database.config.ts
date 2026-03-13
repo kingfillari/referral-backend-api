0d04426f6118cb33b89cd16570891b3039081af6
KingFillari <gebrefillimon@gmail.com>
2026-03-13 16:36:41 +0300

    update src/config/database.config.ts

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,6 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { Patient } from '../patients/entities/patient.entity';
 import { User } from '../users/entities/user.entity';
+import { Patient } from '../patients/entities/patient.entity';
 import { Hospital } from '../hospitals/entities/hospital.entity';
 import { Referral } from '../referrals/entities/referral.entity';
 import { Appointment } from '../appointments/entities/appointment.entity';
@@ -9,9 +9,17 @@ import { Comment } from '../comments/entities/comment.entity';
 export const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  port: parseInt(process.env.DB_PORT, 10) || 5432,
+  port: parseInt(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'rms_db',
   entities: [User, Patient, Hospital, Referral, Appointment, Comment],
   synchronize: true,
+  logging: true,
+  cache: true,
+  migrationsTableName: 'migrations',
+  migrations: ['dist/migrations/*.js'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
+};
